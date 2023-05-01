b48acecc9567eb7eb0d1419d0b190aa60d62d97c
valter-junior <valterjose.sjr@gmail.com>
2023-05-01 15:23:07 -0300

    Added the controllers.

---
+++ backend/hahn/hahn.Application/Controllers/OrderController.cs
@@ -0,0 +1,18 @@
+﻿using hahn.Service.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hahn.Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize("Bearer")]
+    public class OrderController: ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+    }
+}
