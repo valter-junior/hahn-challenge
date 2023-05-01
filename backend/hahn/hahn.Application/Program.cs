53f6fc65e1429b5155a9f2e2f8b8f121c16f9a8d
valter-junior <valterjose.sjr@gmail.com>
2023-05-01 18:59:57 -0300

    Added the database connection.

--- backend/hahn/hahn.Application/Program.cs
+++ backend/hahn/hahn.Application/Program.cs
@@ -1,7 +1,87 @@
+﻿using hahn.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Text;
+using hahn.Service.Services;
+using hahn.Domain.Entities;
+using hahn.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
+string security_key = builder.Configuration.GetSection("TokenAuthentication")["SecretKey"];
+var symetricSecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(security_key));
+
+string connection = builder.Configuration.GetConnectionString("localMySqlConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(connection, ServerVersion.AutoDetect(connection), b => b.MigrationsAssembly("hahn.Infrastructure")));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.TokenValidationParameters.ValidateIssuerSigningKey = true;
+    options.TokenValidationParameters.IssuerSigningKey = symetricSecurityKey;
+    options.TokenValidationParameters.ValidAudience = builder.Configuration.GetSection("TokenAuthentication")["Audience"];
+    options.TokenValidationParameters.ValidIssuer = builder.Configuration.GetSection("TokenAuthentication")["Issuer"];
+    options.TokenValidationParameters.ClockSkew = TimeSpan.Zero;
+});
+
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder => builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+    );
+});
+
+builder.Services.AddAuthorization(auth =>
+{
+    auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme‌​)
+        .RequireAuthenticatedUser().Build());
+});
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+);
+
+//builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddRazorPages();
+
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IBuyerService, BuyerService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IBuyerRepository, BuyerRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.User.AllowedUserNameCharacters = String.Empty;
+    options.User.RequireUniqueEmail = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+}).AddRoleManager<RoleManager<IdentityRole>>()
+      .AddRoles<IdentityRole>()
+      .AddEntityFrameworkStores<ApplicationDbContext>()
+      .AddDefaultTokenProviders();
+
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
@@ -12,14 +92,40 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseResponseCaching();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.UseCors("CorsPolicy");
+
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
+app.UseSwagger(options =>
+{
+    options.SerializeAsV2 = true;
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseRouting();
+app.MapControllers();
 
-app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
-app.MapRazorPages();
 
 app.Run();
--- backend/hahn/hahn.Domain/Entities/BuyerAggregate/BuyerAddress.cs
+++ backend/hahn/hahn.Domain/Entities/BuyerAggregate/BuyerAddress.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using hahn.Domain.Entities.OrderAggregate;
-using static hahn.Service.Models.BuyerModels;
 
 namespace hahn.Domain.Entities.BuyerAggregate
 {
